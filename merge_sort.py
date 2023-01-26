b6f7ba25e6730ecc47102bbcad173b65690b6595
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 10:04:47 +0800

    def merge

--- merge_sort.py
+++ merge_sort.py
@@ -2,3 +2,5 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
+
+def merge_sort(array):
