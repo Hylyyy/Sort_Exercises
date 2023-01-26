10bf8942128cf92773102498bbdd27d4afc83695
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 10:06:40 +0800

    if len

--- merge_sort.py
+++ merge_sort.py
@@ -4,3 +4,6 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 
 def merge_sort(array):
+    if len(array) > 1:
+        left_array = array[:len(array)//2]
+        right_array = array[len(array)//2:]
