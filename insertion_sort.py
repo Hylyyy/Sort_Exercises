9404cbd241859af06c7d3256d366567eccbef539
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 14:51:48 +0800

    updated

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,7 +2,7 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
-print("\t\tARRAY VALUES")
+print("============= ARRAY VALUES ==============")
 print(numbers)
 print()
 print("=========================================\n")
@@ -13,6 +13,8 @@ def insertion_sort(numbers):
         while numbers[j - 1] > numbers[j] and j > 0:
             numbers[j - 1], numbers[j] = numbers[j], numbers[j - 1]
             j -= 1
+            print(numbers)
 
 insertion_sort(numbers)
+print("\n============= SORTED ARRAYS =============\n")
 print(numbers)
