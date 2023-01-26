ad42de38860b459c889153995d5e1f7d5d6dc027
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 09:35:31 +0800

    for loop

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,3 +4,5 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 
 def insertion_sort(numbers):
+    for h in range(1, len(numbers)):
+        j = h
