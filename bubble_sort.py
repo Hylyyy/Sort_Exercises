7cd99b45f467391c8732861113eadc5663872d4c
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 08:59:02 +0800

    h and j range

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,4 +4,5 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 
 def sort(numbers):
-    
+    for h in range(len(numbers)-1,0,-1):
+        for j in range(h):
