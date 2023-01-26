460d09ef1c50f7faaacad005d50847fc865c5b73
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 10:59:16 +0800

    partition position

--- quick_sort.py
+++ quick_sort.py
@@ -5,7 +5,10 @@ numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 
 def quicksort(array, left, right):
     if left < right:
-        
+        partition_position = partition(array, left, right)
+        quicksort(array, left, partition_position - 1)
+        quicksort(array, partition_position + 1, right)
+                
 
 def partition(array, left, right):
     h = left
