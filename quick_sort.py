3bec4e744e34a2e96c7776e58c0d5f512563e521
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 14:15:07 +0800

    fixing

--- quick_sort.py
+++ quick_sort.py
@@ -3,31 +3,31 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 
-def quicksort(array, left, right):
+def quicksort(numbers, left, right):
     if left < right:
-        partition_position = partition(array, left, right)
-        quicksort(array, left, partition_position - 1)
-        quicksort(array, partition_position + 1, right)
+        partition_position = partition(numbers, left, right)
+        quicksort(numbers, left, partition_position - 1)
+        quicksort(numbers, partition_position + 1, right)
                 
 
-def partition(array, left, right):
+def partition(numbers, left, right):
     h = left
     j = right - 1
-    pivot = array[right]
+    pivot = numbers[right]
 
     while h < j:
-        while h < right and array[h] < pivot:
+        while h < right and numbers[h] < pivot:
             h += 1
-        while j > left and array[j] >= pivot:
+        while j > left and numbers[j] >= pivot:
             j -= 1
         if h < j:
-            array[h], array[j] = array[h], array[h]
+            numbers[h], numbers[j] = numbers[h], numbers[h]
 
-    if array[h] > pivot:
-        array[h], array[right] = array[right], array[h]
+    if numbers[h] > pivot:
+        numbers[h], numbers[right] = numbers[right], numbers[h]
 
     return h
 
-quicksort(array, 0, len(array) -1)
+quicksort(numbers, 0, len(numbers) -1)
 
-print(array)
+print(numbers)
