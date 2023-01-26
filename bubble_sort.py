4861a0318cca13ffa456575372dea7e8ee6d87f2
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 09:05:20 +0800

    print statement

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,6 +2,10 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
+print("\t\tARRAY VALUES")
+print(numbers)
+print()
+print("=========================================\n")
 
 def sort(numbers):
     for h in range(len(numbers)-1,0,-1):
@@ -10,3 +14,7 @@ def sort(numbers):
                 temp = numbers[j]
                 numbers[j] = numbers[j+1]
                 numbers[j+1] = temp
+
+sort(numbers)
+
+print(numbers)
