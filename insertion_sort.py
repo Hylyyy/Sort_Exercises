89a0ab7c3c5b80ebc900cf21274fe999691ade68
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 09:38:31 +0800

    while loop

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,3 +6,6 @@ numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
 def insertion_sort(numbers):
     for h in range(1, len(numbers)):
         j = h
+        while numbers[j - 1] > numbers[j] and j > 0:
+            numbers[j - 1], numbers[j] = numbers[j], numbers[j - 1]
+            j -= 1
