67429a1cf878582b3c82920ad399fa37d9e69a5e
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-26 10:53:19 +0800

    def quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -2,3 +2,7 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 numbers = [51, 30, 69, 100, 46, 63, 41, 19, 98, 49]
+
+def quicksort(array, left, right):
+    if left < right:
+        
